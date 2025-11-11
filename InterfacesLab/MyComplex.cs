bc18b3be59f3d3ac4a05d066911daf88f427ab82
Mozgushin Vitalii <mozhushyn.vitalii1124@vu.cdu.edu.ua>
2025-11-12 01:57:41 +0200

    Created main project, added MyComplex, MyFrac classes,Main,added IMyNumber interface

---
+++ InterfacesLab/MyComplex.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab;
+
+public class MyComplex : IMyNumber<MyComplex>
+{
+    public MyComplex Add(MyComplex b)
+    {
+        throw new NotImplementedException();
+    }
+
+    public MyComplex Divide(MyComplex b)
+    {
+        throw new NotImplementedException();
+    }
+
+    public MyComplex Multiply(MyComplex b)
+    {
+        throw new NotImplementedException();
+    }
+
+    public MyComplex Substract(MyComplex b)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ InterfacesLab/MyFrac.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace Lab
+{
+    public class MyFrac : IMyNumber<MyFrac>
+    {
+        public MyFrac Add(MyFrac b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MyFrac Divide(MyFrac b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MyFrac Multiply(MyFrac b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MyFrac Substract(MyFrac b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
